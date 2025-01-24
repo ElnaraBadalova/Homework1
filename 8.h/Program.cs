e974e1881d7397efb0404ef0bbc644d992fb017e
ElnaraBadalova <elnarahb@code.edu.az>
2025-01-24 16:04:06 +0400

    homework cont.

---
+++ 8.h/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8.h
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter pass");
+            string a = Console.ReadLine();
+            string b = "Pb403";
+            if (a == b)
+            { Console.WriteLine("correct password"); }
+            else { Console.WriteLine("incorrect password"); }
+
+
+
+
+        }
+    }
+}
